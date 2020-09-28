8654498cca8f99a641382b0505b4c352f7c9ae07
Debora Silva <debora.rss22@gmail.com>
2020-09-28 18:43:36 -0300

    Adjust content register form

--- src/components/RegisterForm/index.js
+++ src/components/RegisterForm/index.js
@@ -1,20 +1,94 @@
 import React from "react";
-import { Steps, Button, message } from "antd";
+import { Steps, Button, message, Form, Input, Select } from "antd";
 
 const { Step } = Steps;
+
+const FirstName = () => (
+  <Form.Item
+    name="firstname"
+    rules={[
+      {
+        required: true,
+        message: "Por favor, digite seu nome!",
+      },
+    ]}
+  >
+    <Input placeholder="Digite seu nome" />
+  </Form.Item>
+);
+
+const LastName = () => (
+  <Form.Item
+    name="lastname"
+    rules={[
+      {
+        required: true,
+        message: "Por favor, digite seu sobrenome!",
+      },
+    ]}
+  >
+    <Input placeholder="Digite seu sobrenome" />
+  </Form.Item>
+);
+
+const Password = () => (
+  <Form.Item
+    name="password"
+    rules={[
+      {
+        required: true,
+        message: "Por favor, digite seu senha!",
+      },
+    ]}
+  >
+    <Input placeholder="Digite sua senha" />
+  </Form.Item>
+);
+
+const Currency = () => (
+  <Form.Item name="gender" rules={[{ required: true }]}>
+    <Select placeholder="Selecione uma opção" allowClear>
+      <Select.Option value="male">Masculino</Select.Option>
+      <Select.Option value="female">Feminino</Select.Option>
+      <Select.Option value="other">Outro</Select.Option>
+    </Select>
+  </Form.Item>
+);
+
+const EmailAddress = () => (
+  <Form.Item
+    name="email"
+    rules={[
+      {
+        required: true,
+        message: "Por favor, digite seu e-mail!",
+      },
+    ]}
+  >
+    <Input placeholder="Digite seu e-mail" />
+  </Form.Item>
+);
 
 const steps = [
   {
     title: "Nome",
+    content: <FirstName />,
   },
   {
     title: "Sobrenome",
+    content: <LastName />,
   },
   {
     title: "Senha",
+    content: <Password />,
+  },
+  {
+    title: "Moeda",
+    content: <Currency />,
   },
   {
     title: "E-mail",
+    content: <EmailAddress />,
   },
 ];
 
@@ -32,6 +106,10 @@ const RegisterForm = () => {
           <Step key={step.title} title={step.title} />
         ))}
       </Steps>
+
+      <Form name="register">
+        <div>{steps[currentStep].content}</div>
+      </Form>
 
       <div style={{ marginTop: "30px" }}>
         {currentStep > 0 ? (
